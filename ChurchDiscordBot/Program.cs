19ff4d99ec469131099aa4ef9d4b0e33d937870b
Troy Miller <yort5@hotmail.com>
2023-12-06 09:02:47 -0600

    Fix fat finger from last minute late night push.

--- ChurchDiscordBot/Program.cs
+++ ChurchDiscordBot/Program.cs
@@ -5,7 +5,7 @@ using Microsoft.Extensions.Configuration.AzureAppConfiguration;
 var builder = Host.CreateApplicationBuilder(args);
 
 // Retrieve the connection string
-string connectionString = builder.Configuration.GetConnectionString("Azure:AppConfiguration") ?? string.Empty;
+var connectionString = builder.Configuration.GetValue<string>("Azure:AppConfiguration:ConnectionString") ?? string.Empty;
 
 // Load configuration from Azure App Configuration
 builder.Configuration.AddAzureAppConfiguration(options =>
